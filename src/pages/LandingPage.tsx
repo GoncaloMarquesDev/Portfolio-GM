0c8469db702be27994e36e79aa47743aac4bbac8
goncalo marques <goncalomarques112@gmail.com>
2025-12-10 11:04:24 +0000

    Error resolved in deployment

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,7 +8,6 @@ import Resume from "../components/resume/Resume";
 
 import "./LandingPage.scss";
 import Footer from "../components/footer/Footer";
-import CardV2 from "../components/cardv2/CardV2";
 
 function LandingPage() {
   const portfolioRef = useRef<HTMLDivElement | null>(null);
@@ -21,31 +20,30 @@ function LandingPage() {
 
   return (
     <>
-    <div className="teste">
-      <div className="page-wrapper">
-        <Nav
-          onProjectsClick={() => scrollToRef(portfolioRef)}
-          onAboutClick={() => scrollToRef(aboutRef)}
-          onResumeClick={() => scrollToRef(resumeRef)}
-        />
-        <Hero />
-        <div ref={aboutRef}>
-          <AboutMe />
-        </div>
-        <div ref={portfolioRef}>
-          <PortfolioSection />
-        </div>
-        <CardV2/>
-        <div ref={resumeRef}>
-          <Resume />
-        </div>
-        <Footer
-  onProjectsClick={() => scrollToRef(portfolioRef)}
-  onAboutClick={() => scrollToRef(aboutRef)}
-  onResumeClick={() => scrollToRef(resumeRef)}
-/>
+      <div className="teste">
+        <div className="page-wrapper">
+          <Nav
+            onProjectsClick={() => scrollToRef(portfolioRef)}
+            onAboutClick={() => scrollToRef(aboutRef)}
+            onResumeClick={() => scrollToRef(resumeRef)}
+          />
+          <Hero />
+          <div ref={aboutRef}>
+            <AboutMe />
+          </div>
+          <div ref={portfolioRef}>
+            <PortfolioSection />
+          </div>
 
-      </div>
+          <div ref={resumeRef}>
+            <Resume />
+          </div>
+          <Footer
+            onProjectsClick={() => scrollToRef(portfolioRef)}
+            onAboutClick={() => scrollToRef(aboutRef)}
+            onResumeClick={() => scrollToRef(resumeRef)}
+          />
+        </div>
       </div>
     </>
   );
